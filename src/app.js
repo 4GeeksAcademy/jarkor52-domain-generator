a3da4b06c7a3d623ee613f1af4f2e2f6078436be
jarkor5 <67030533+jarkor5@users.noreply.github.com>
2023-07-11 02:17:22 +0000

    domain generator ready

--- src/app.js
+++ src/app.js
@@ -1,11 +1,51 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+// script.js
+let pronoun = [
+  "el",
+  "nuestro",
+  "mi",
+  "tu",
+  "su",
+  "ese",
+  "mi propio",
+  "cada",
+  "todo",
+  "algún",
+];
+let adj = [
+  "gran",
+  "grande",
+  "increíble",
+  "pequeño",
+  "hermoso",
+  "rojo",
+  "brillante",
+  "fantastico",
+  "poderoso",
+  "sabio",
+];
+let noun = [
+  "corredor",
+  "mapache",
+  "sitio",
+  "ciudad",
+  "proyecto",
+  "jardín",
+  "tesoro",
+  "robot",
+  "pensamiento",
+  "destino",
+];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let domainList = document.getElementById("domainList");
+let domainHTML = "";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let k = 0; k < noun.length; k++) {
+      let domain = pronoun[i] + adj[j] + noun[k] + ".com";
+      domainHTML += "<li>" + domain + "</li>";
+    }
+  }
+}
+
+domainList.innerHTML = domainHTML;
